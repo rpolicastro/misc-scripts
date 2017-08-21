5390934643498961c8165ddce99633d86a12ccd9
rpolicastro <31133814+rpolicastro@users.noreply.github.com>
2017-08-21 10:17:27 -0400

    making it an object

--- peaks2bed.py
+++ peaks2bed.py
@@ -1,6 +1,8 @@
 ##############
-#change peak_folder to the folder where your peak files are in,
-#or put script in peak file directory
+#convert homer peak files to bed files
+#change peak_folder to the folder where your peak files are in
+#or
+#put script in peak file directory
 #need to import module python
 ##############
 
@@ -8,53 +10,64 @@ import pandas as pd
 import os
 import sys
 
-###variables
+###classes
 
-peak_folder = ''
-
-###script
-
-if peak_folder == '':
-    peak_folder = os.getcwd()
-
-output_dir = peak_folder + 'cleaned/'
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
-
-#clean header
-
-file_list = os.listdir(peak_folder)
-peak_files = [x for x in file_list if x.endswith('.txt')]
-
-if len(peak_files) == 0:
-    sys.exit('no peak files found')    
-
-for p in peak_files:
+class peaks2bed(object):
+        
+    def __init__(self, peak_folder=''):
+        
+        self.peak_folder = peak_folder
+        
+        if self.peak_folder == '':
+            self.peak_folder = os.getcwd()
     
-    peak_file = peak_folder + p
-    with open(peak_file) as f:
-        cleaned = [x.rstrip() for x in f if x.startswith('#PeakID') or not x.startswith('#')]
-    
-    new_file = output_dir + 'cleaned_' + p
-    if not os.path.isfile(new_file):
-        contents = '\n'.join(cleaned)
-        with open(new_file, mode='a') as f:
-            f.write(contents)
-    
-#format as bed
+    def convert(self):
+        
+        output_dir = self.peak_folder + 'cleaned/'
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        
+        #clean header
+        
+        file_list = os.listdir(self.peak_folder)
+        peak_files = [x for x in file_list if x.endswith('.txt')]
+        
+        if len(peak_files) == 0:
+            sys.exit('no peak files found')    
+        
+        for p in peak_files:
+            
+            peak_file = self.peak_folder + p
+            with open(peak_file) as f:
+                cleaned = [x.rstrip() for x in f if x.startswith('#PeakID') or not x.startswith('#')]
+            
+            new_file = output_dir + 'cleaned_' + p
+            if not os.path.isfile(new_file):
+                contents = '\n'.join(cleaned)
+                with open(new_file, mode='a') as f:
+                    f.write(contents)
+            
+        #format as bed
+        
+        output_dir = self.peak_folder + 'beds/'
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        
+        file_list = os.listdir(str(self.peak_folder + 'cleaned/'))
+        peak_files = [x for x in file_list if x.startswith('cleaned_') and x.endswith('.txt')]
+        
+        for p in peak_files:
+            
+            peaks = pd.read_table(str(self.peak_folder + 'cleaned/' + p), sep='\t')
+            filtered = peaks[['chr','start','end','#PeakID','findPeaks Score','strand']]
+            
+            new_file = output_dir + p[8:-4] + '.bed'
+            if not os.path.isfile(new_file):
+                filtered.to_csv(new_file, sep='\t', index=False, header=False)
 
-output_dir = peak_folder + 'beds/'
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
+###run program
 
-file_list = os.listdir(str(peak_folder + 'cleaned/'))
-peak_files = [x for x in file_list if x.startswith('cleaned_') and x.endswith('.txt')]
-
-for p in peak_files:
+if __name__ == '__main__':
     
-    peaks = pd.read_table(str(peak_folder + 'cleaned/' + p), sep='\t')
-    filtered = peaks[['chr','start','end','#PeakID','findPeaks Score','strand']]
-    
-    new_file = output_dir + p[8:-4] + '.bed'
-    if not os.path.isfile(new_file):
-        filtered.to_csv(new_file, sep='\t', index=False, header=False)
+    convert_peaks = peaks2bed(peak_folder='')
+    convert_peaks.convert()
